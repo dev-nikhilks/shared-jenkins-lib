25b699b68c5b15a82c4418557d7bd813d251608a
Nikhil KS <Nikhil.Soman>
2024-12-19 22:07:11 +0530

    overall changes

--- vars/updateConfluence.groovy
+++ vars/updateConfluence.groovy
@@ -1,88 +1,56 @@
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
-// Function to set the values to confluence
-def call(pageId, project, releaseVersion, type, android, ios){
-    println "Confluence Start Fetch"
-    def pageData = fetchPageContent(pageId);
-    println "Confluence Initial Fetch"
-    if( pageData != null ) {
-        def currentVersion = pageData.results[0].version.number
-        def pageTitle = pageData.results[0].title
-        def pageBody = pageData.results[0].body.storage.value
+def call(Map<String, String> inputs){
+    def pageContent = getConfluencePageContent(inputs.pageId);
+    def currentPageVersion = pageContent.version.number
+    def content = pageContent.body.storage.value
+    def title = pageContent.title
 
-        def updatedBody = appendRowToSpecificTable(pageBody, project, releaseVersion, type, android, ios)
-        // updateConfluencePage(pageId, pageTitle, pageBody, currentVersion)
-        println "Confluence Success"
-    } else {
-        println "Confluence Page Not found"
-    }
-}
+    def config = [
+        'customer': inputs.customer,
+        'appVersion': inputs.appVersion,
+        'pageVersion': currentPageVersion,
+        'androidLink': inputs.androidLink,
+        'iosLink': inputs.iosLink,
+        'title': title,
+        'pageId': inputs.pageId
+    ]
 
-// Function to fetch the current page content
-def fetchPageContent(pageId) {
-    def url = "${env.CONFLUENCE_BASE_URL}/pages?id=${pageId}&body-format=storage"
-    def connection = new URL(url).openConnection()
-    connection.setRequestProperty("Authorization", "Basic " + "${CONF_USERNAME}:${CONF_PASSWORD}".bytes.encodeBase64().toString())
-    connection.setRequestProperty("Content-Type", "application/json")
-    connection.setRequestMethod("GET")
-    
-    try {
-        def responseCode = connection.responseCode
-        if (responseCode == 200) {
-            def response = connection.inputStream.text
-            return new JsonSlurper().parseText(response)
-        } else {
-            def errorMessage = connection.errorStream?.text ?: "Unknown error"
-            println "Failed to fetch Confluence page content. HTTP ${responseCode}: ${errorMessage}"
-            return null
-        }
-    } catch (Exception e) {
-        println "Error occurred while fetching Confluence page content: ${e.message}"
-        return null
-    }
-}
-
-// Function to append a row to a specific table identified by ac:local-id
-def appendRowToSpecificTable(bodyContent, targetTableId, version, customer, androidLink, iosLink) {
-    // def newRow = buildNewTableRow(version, customer, androidLink, iosLink)
-
-    // // Locate the target table using ac:local-id
-    // def updatedBody = bodyContent.replaceFirst(/(<table[^>]*ac:local-id="${targetTableId}"[^>]*>.*?<tbody>)(.*?)(<\/tbody>)/) { match, beforeBody, rows, afterBody ->
-    //     "${beforeBody}${rows}${newRow}${afterBody}"
-    // }
-
-    def body = "<ac:layout><ac:layout-section ac:type=\"two_equal\" ac:breakout-mode=\"default\"><ac:layout-cell><h3>iTravel POS and Dining</h3><table data-table-width=\"760\" data-layout=\"center\" ac:local-id=\"itravel-pos-and-dining\"><colgroup><col style=\"width: 77.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /></colgroup><tbody><tr><th><p style=\"text-align: center;\"><strong>Date</strong></p></th><th><p style=\"text-align: center;\"><strong>Type</strong></p></th><th><p style=\"text-align: center;\"><strong>Version</strong></p></th><th><p style=\"text-align: center;\"><strong>Android</strong></p></th><th><p style=\"text-align: center;\"><strong>iOS</strong></p></th></tr><tr><td><p style=\"text-align: center;\">22/11/2024</p></td><td><p style=\"text-align: center;\">RCYC</p></td><td><p style=\"text-align: center;\">1.0.3(1030)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr><tr><td><p style=\"text-align: center;\">22/11/2024</p></td><td><p style=\"text-align: center;\">RCYC</p></td><td><p style=\"text-align: center;\">1.0.3(1030)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr><tr><td><p style=\"text-align: center;\">22/10/2024</p></td><td><p style=\"text-align: center;\">Internal</p></td><td><p style=\"text-align: center;\">1.0.2(1020)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr></tbody></table></ac:layout-cell><ac:layout-cell><h3>iTravel Crew Navigator</h3><table data-table-width=\"760\" data-layout=\"center\" ac:local-id=\"itravel-crew-navigator\"><colgroup><col style=\"width: 77.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /></colgroup><tbody><tr><th><p style=\"text-align: center;\"><strong>Date</strong></p></th><th><p style=\"text-align: center;\"><strong>Type</strong></p></th><th><p style=\"text-align: center;\"><strong>Version</strong></p></th><th><p style=\"text-align: center;\"><strong>Android</strong></p></th><th><p style=\"text-align: center;\"><strong>iOS</strong></p></th></tr><tr><td><p style=\"text-align: center;\">11/12/2023</p></td><td><p style=\"text-align: center;\">Internal</p></td><td><p style=\"text-align: center;\">1.0.2(1020)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr></tbody></table></ac:layout-cell></ac:layout-section><ac:layout-section ac:type=\"two_equal\" ac:breakout-mode=\"default\"><ac:layout-cell><h3>iTravel Check-in </h3><table data-table-width=\"760\" data-layout=\"center\" ac:local-id=\"itravel-check-in\"><colgroup><col style=\"width: 77.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /></colgroup><tbody><tr><th><p style=\"text-align: center;\"><strong>Date</strong></p></th><th><p style=\"text-align: center;\"><strong>Type</strong></p></th><th><p style=\"text-align: center;\"><strong>Version</strong></p></th><th><p style=\"text-align: center;\"><strong>Android</strong></p></th><th><p style=\"text-align: center;\"><strong>iOS</strong></p></th></tr><tr><td><p style=\"text-align: center;\">10/11/2023</p></td><td><p style=\"text-align: center;\">Internal</p></td><td><p style=\"text-align: center;\">1.0.2(1020)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr><tr><td><p style=\"text-align: center;\">10/12/2024</p></td><td><p style=\"text-align: center;\">RCYC</p></td><td><p style=\"text-align: center;\">1.0.3(1030)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr></tbody></table></ac:layout-cell><ac:layout-cell><h3>iTravel Guest Services</h3><table data-table-width=\"760\" data-layout=\"center\" ac:local-id=\"itravel-guest-services\"><colgroup><col style=\"width: 77.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /><col style=\"width: 71.0px;\" /></colgroup><tbody><tr><th><p style=\"text-align: center;\"><strong>Date</strong></p></th><th><p style=\"text-align: center;\"><strong>Type</strong></p></th><th><p style=\"text-align: center;\"><strong>Version</strong></p></th><th><p style=\"text-align: center;\"><strong>Android</strong></p></th><th><p style=\"text-align: center;\"><strong>iOS</strong></p></th></tr><tr><td><p style=\"text-align: center;\">12/11/2024</p></td><td><p style=\"text-align: center;\">Internal</p></td><td><p style=\"text-align: center;\">1.0.2(1020)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr><tr><td><p style=\"text-align: center;\">12/03/2024</p></td><td><p style=\"text-align: center;\">RCYC</p></td><td><p style=\"text-align: center;\">1.0.3(1030)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr><tr><td><p style=\"text-align: center;\">22/11/2024</p></td><td><p style=\"text-align: center;\">RCYC</p></td><td><p style=\"text-align: center;\">1.0.3(1030)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr><tr><td><p style=\"text-align: center;\">22/11/2024</p></td><td><p style=\"text-align: center;\">RCYC</p></td><td><p style=\"text-align: center;\">1.0.3(1030)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr><tr><td><p style=\"text-align: center;\">22/11/2024</p></td><td><p style=\"text-align: center;\">RCYC</p></td><td><p style=\"text-align: center;\">1.0.3(1030)</p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td><td><p style=\"text-align: center;\"><a href=\"https://www.google.com\">Download</a></p></td></tr></tbody></table></ac:layout-cell></ac:layout-section></ac:layout>"
-    def newRowData = ["New Value 1", "New Value 2", "New Value 3","New Value 4", "New Value 5"]
- 
-    // if (updatedBody == bodyContent) {
-    //     error "No table with ac:local-id '${targetTableId}' was found."
-    // }
-    def doc = org.jsoup.Jsoup.parse(body)
-    def table = doc.select("table[ac:local-id=itravel-pos-and-dining]").first()
+    def newRow = buildNewTableRow(config)
+    def doc = org.jsoup.Jsoup.parse(content)
+    def table = doc.select("table[ac:local-id=${inputs.app-id}]").first()
     if (!table) {
         println "Table not found"
         return null
     }
     def tbody = table.select("tbody").first()
-    if (!tbody) {
-        println "Table body not found"
-        return null
-    }
-        // Create the new table row
-    def newRow = tbody.append("<tr></tr>").select("tr").last()
-
-    // Add the cells to the new row
-    newRowData.each { cellData ->
-        newRow.append("<td>${cellData}</td>")
-    }
-    // table.append("${newRow}")
-    println "Confluence page data is ${doc.html()}"
-    return null
+    tbody.append(newRow)
+    updateConfluencePageContent(config, doc.html())
 }
 
-// Function to build a new table row
-def buildNewTableRow(version, customer, androidLink, iosLink) {
+def getConfluencePageContent(pageId) {
+    def cmd = ["curl", "-s", "-u", "${CONF_USERNAME}:${CONF_PASSWORD}", "${env.CONFLUENCE_BASE_URL}/pages?id=${pageId}&body-format=storage"]
+    def process = cmd.execute()
+    process.waitFor()
+    if (process.exitValue() != 0) {
+        throw new RuntimeException("Failed to get Confluence page: ${process.err.text}")
+    }
+    return new JsonSlurper().parseText(process.text)
+}
+
+def updateConfluencePageContent(Map<String, String> inputs, newContent) {
+    def jsonBuilder = new JsonBuilder([version: [number: inputs.pageVersion + 1], title: inputs.title, id: inputs.pageId, status: "current", body: [representation: "storage", value: newContent]])
+    def cmd = ["curl", "-X", "PUT", "-s", "-u", "${CONF_USERNAME}:${CONF_PASSWORD}", "-H", "Content-Type: application/json", "-d", jsonBuilder.toPrettyString(), "${env.CONFLUENCE_BASE_URL}/pages/${inputs.pageId}"]
+    def process = cmd.execute()
+    process.waitFor()
+    if (process.exitValue() != 0) {
+        throw new RuntimeException("Failed to update Confluence page: ${process.err.text}")
+    }
+    println process.text
+}
+
+def buildNewTableRow(Map<String, String> inputs) {
     def currentDate = new Date().format("dd/MM/yyyy")
     return """
         <tr>
@@ -90,62 +58,17 @@ def buildNewTableRow(version, customer, androidLink, iosLink) {
                 <p style="text-align: center;">${currentDate}</p>
             </td>
             <td>
-                <p style="text-align: center;">${customer}</p>
+                <p style="text-align: center;">${inputs.customer}</p>
             </td>
             <td>
-                <p style="text-align: center;">${version}</p>
+                <p style="text-align: center;">${inputs.appVersion}</p>
             </td>
             <td>
-                <p style="text-align: center;"><a href="${androidLink}">Download</a></p>
+                <p style="text-align: center;"><a href="${inputs.androidLink}">Download</a></p>
             </td>
             <td>
-                <p style="text-align: center;"><a href="${iosLink}">Download</a></p>
+                <p style="text-align: center;"><a href="${inputs.iosLink}">Download</a></p>
             </td>
         </tr>
     """
 }
-
-// Function to update the Confluence page
-def updateConfluencePage(pageId, title, updatedBody, currentVersion) {
-    def url = "${env.CONFLUENCE_BASE_URL}/pages/${pageId}"
-    def payload = [
-        version: [number: currentVersion + 1],
-        title  : title,
-        id: pageId,
-        status: "current",
-        body   : [
-            representation: "storage",
-            value: updatedBody
-        ]
-    ]
-
-    println "Error occurred while fetching Confluence page content: ${updatedBody}"
-
-    def connection = new URL(url).openConnection()
-    connection.setRequestProperty("Authorization", "Basic " + "${CONF_USERNAME}:${CONF_PASSWORD}".bytes.encodeBase64().toString())
-    connection.setRequestProperty("Content-Type", "application/json")
-    connection.setRequestMethod("PUT")
-    connection.doOutput = true
-
-    def writer = new OutputStreamWriter(connection.outputStream)
-    writer.write(JsonOutput.toJson(payload))
-    writer.flush()
-    writer.close()
-    println "Error occurred while fetching Confluence page content: ${JsonOutput.toJson(payload)}"
-    try {
-        def responseCode = connection.responseCode
-        if (responseCode == 200) {
-            def response = connection.inputStream.text
-            return new JsonSlurper().parseText(response)
-        } else {
-            def errorMessage = connection.errorStream?.text ?: "Unknown error"
-            println "Failed to fetch Confluence page content. HTTP ${responseCode}: ${errorMessage}"
-            return null
-        }
-    } catch (Exception e) {
-        println "Error occurred while fetching Confluence page content: ${e.message}"
-        return null
-    }
-}
-
-return this
