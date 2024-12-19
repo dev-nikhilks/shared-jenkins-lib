224cc663146708cf28b21e83c90a1bda7f4f8bdc
Nikhil KS <Nikhil.Soman>
2024-12-19 23:34:03 +0530

    Empty check

--- vars/updateConfluence.groovy
+++ vars/updateConfluence.groovy
@@ -44,7 +44,7 @@ def getConfluencePageContent(pageId) {
         return null
     }
     def response = new JsonSlurper().parseText(process.text)
-    if(response.results.length() == 0){
+    if(response.results.isEmpty()){
         return null;
     }
     return response;
